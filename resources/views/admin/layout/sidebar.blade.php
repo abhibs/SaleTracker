61a6e050a1a2fcf93c091e43be612ed8f621902d
abhibs <abhiram.bs@webdigitalmantra.in>
2025-09-05 21:47:33 +0530

    admin dashboard page created

---
+++ resources/views/admin/layout/sidebar.blade.php
@@ -0,0 +1,28 @@
+    <aside class="sidebar-wrapper" data-simplebar="true">
+        <div class="sidebar-header">
+            {{-- <div class="logo-icon">
+                <img src="assets/images/logo-icon.png" class="logo-img" alt="">
+            </div> --}}
+            <div class="logo-name flex-grow-1">
+                <h5 class="mb-0">Admin Dashboard</h5>
+            </div>
+            <div class="sidebar-close">
+                <span class="material-icons-outlined">close</span>
+            </div>
+        </div>
+        <div class="sidebar-nav">
+            <!--navigation-->
+            <ul class="metismenu" id="sidenav">
+                <li>
+                    <a href="{{ route('admin-dashboard') }}">
+                        <div class="parent-icon"><i class="material-icons-outlined">home</i>
+                        </div>
+                        <div class="menu-title">Dashboard</div>
+                    </a>
+
+                </li>
+
+            </ul>
+            <!--end navigation-->
+        </div>
+    </aside>
