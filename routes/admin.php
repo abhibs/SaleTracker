2ef1ca74e3bdecc7c026dd719b7487d70fa93875
abhibs <abhiram.bs@webdigitalmantra.in>
2025-09-05 21:03:19 +0530

    admin login page created

--- routes/admin.php
+++ routes/admin.php
@@ -1,7 +1,42 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminController;
 
 Route::get('/test', function () {
     echo "Abhiram";
 });
+
+
+Route::get('/clear-cache', function () {
+    Artisan::call('cache:clear');
+    Artisan::call('config:clear');
+    Artisan::call('view:clear');
+    return "Cache is cleared";
+});
+Route::get('migrate', function () {
+    Artisan::call('migrate');
+    return "Migrate Completed!";
+});
+Route::get('optimize', function () {
+    Artisan::call('optimize');
+    return "optimized!";
+});
+
+
+Route::group(
+    ['prefix' => 'admin'],
+    function () {
+        Route::controller(AdminController::class)->group(
+            function () {
+                Route::get('/login', 'login')->name('admin-login');
+
+                });
+            }
+        );
+
+
+
+
+
+
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
+
+
 Route::get('', [UserController::class, 'userLogin'])->name('login');
 Route::get('register', [UserController::class, 'userRegister'])->name('register');
 Route::post('register', [UserController::class, 'userRegisterPost'])->name('user-register-post');;
